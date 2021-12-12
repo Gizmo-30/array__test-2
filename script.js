8fd825f78d01671f852ba2fd0605cc1bcec34ee0
Gizmo-30 <anvaradizov80@gmail.com>
2021-12-12 23:14:52 +0500

    first

---
+++ script.js
@@ -0,0 +1,21 @@
+let userInfo = []
+
+for (let i = 0; i < Infinity; i++) {
+    let input = prompt('')
+    let res = input.split(' ')
+    if (res[0] == 'add' || res[0] == 'add,') {
+        userInfo.push(res[1])
+        console.log(`элемент ${res[1]} был добавлен в массив`);
+    } else if (res[0] == 'del' || res[0] == 'del,') {
+        for (const key in userInfo) {
+            if (userInfo[key] == res[1]) {
+                userInfo.splice(key, 1)
+            }
+        }
+        console.log(`элемент ${res[1]} был удален из массива`);
+    }
+    if (input == 'stop') {
+        break
+    }
+}
+console.log(userInfo);
