e006ac1ec624b7bfeed4fe12f31c3dad7b3b6555
Gizmo-30 <anvaradizov80@gmail.com>
2021-12-13 15:37:27 +0500

    first

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ for (let i = 0; i < Infinity; i++) {
         for (const key in userInfo) {
             if (userInfo[key] == res[1]) {
                 userInfo.splice(key, 1)
+                console.log(`элемент ${res[1]} был удален из массива`);
             }
         }
-        console.log(`элемент ${res[1]} был удален из массива`);
     } else if (input == 'stop' || input == 'Stop') {
         break
     } else {
